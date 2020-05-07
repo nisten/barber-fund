4478bda0ae50b2329431fff93d1ca1340e3878a7
nisten <nisten@outlook.com>
2020-05-07 17:24:35 -0400

    replaced gatsby with nextjs

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Row, Col, Image } from 'react-bootstrap'
+
+export default function Home() {
+  return (
+    <Row fluid>
+      
+        <Image src='/barberfund.png' style={{ width: '20%' }} fluid></Image>
+      
+      <h3>Welcome to the barberfund</h3>
+    </Row>
+  )
+}
