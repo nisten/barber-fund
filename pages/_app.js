737ad9f8e7b027e538f8c0a8466e57f9189fd33f
nisten <nisten@outlook.com>
2020-05-24 19:27:53 -0400

    nextjs fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import '../styles.css'
+import Head from 'next/head'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  <Head>
+        <meta property="og:title" content="barberfund" key="title" />
+      </Head>
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import {Row, Col, Image } from 'react-bootstrap'
+import React from 'react';
+import { Row, Col, Image } from 'react-bootstrap';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <Row>
-      
-        <Image src='/barberfund.png' style={{ width: '20%' }} fluid></Image>
-      
+      <Head>
+        <html lang="en-us" />
+        <title>barberfund</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="barberfund" key="title"/>
+        {/* <meta charSet="utf-8" /> */}
+        <meta name="description" content="A hackathon app to help you pay it forward to your local barbershop" />
+      </Head>
+
+      <Image
+        src="/barberfund.webp"
+        alt="logo"
+        style={{ width: '20%' }}
+        fluid
+      ></Image>
+
       <h3>Welcome to the barberfund</h3>
     </Row>
-  )
+  );
 }
